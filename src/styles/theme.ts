0304c9fb46a15f68314202f772f8ec04222322f4
kimgyuha <kimgyuha25@gmail.com>
2023-08-28 20:28:02 +0900

    :lipstick: :: color 시스템 구축

---
+++ src/styles/theme.ts
@@ -0,0 +1,20 @@
+export const color = {
+  White: "#FFFFFF",
+  Red: "#CC4E49",
+  Black: {
+    Text: "#000000",
+    Solid: "#1A1A1A",
+  },
+  Gray: {
+    75: "#BFBFBF",
+    85: "#D9D9D9",
+    95: "#F2F2F2",
+  },
+  Blue: {
+    0: "#4F76D6",
+    1: "#5D81D9",
+    2: "#7191DE",
+    8: "#D6E0F5",
+    9: "#E3F2FF",
+  },
+};
