a790694c0c2fbb7c2ac3824c3e3ee51f01c123a4
kimgyuha <kimgyuha25@gmail.com>
2023-09-01 17:28:54 +0900

    :lipstick: :: Icon 추가

---
+++ src/assets/icons/Check.tsx
@@ -0,0 +1,22 @@
+import { IconProps } from "../../types/IconProps";
+
+/** 기본 사이즈 24px (small입력시 20px) */
+export const Check = ({ small }: IconProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      width={small ? 20 : 24}
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M4.5 12.75l6 6 9-13.5"
+      />
+    </svg>
+  );
+};
---
+++ src/assets/icons/ChevronLeft.tsx
@@ -0,0 +1,22 @@
+import { IconProps } from "../../types/IconProps";
+
+/** 기본 사이즈 24px (small입력시 20px) */
+export const ChevronLeft = ({ small }: IconProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      width={small ? 20 : 24}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M15.75 19.5L8.25 12l7.5-7.5"
+      />
+    </svg>
+  );
+};
