9646f388cbac7e19c4e2021df91612d6f7e5d593
김상구 <spair0039@gmail.com>
2023-09-16 15:09:15 +0900

    💄:: 파일 내려받기 기본 파일 생성

---
+++ src/components/document/PDFCerificationOfCareer.tsx
@@ -0,0 +1,11 @@
+import { Document, Page } from "@react-pdf/renderer"
+
+export default function PDFCerificationOfCareer(){
+    return(
+        <Document >
+            <Page size={"A4"} >
+
+            </Page>
+        </Document>
+    )
+}
