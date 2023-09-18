07fd215c284978ded96374a8b91c4e44215191c9
Jin hyeon <kangjin627@gmail.com>
2023-09-18 19:17:32 +0900

    ✨ :: router 주소 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
 import { Route, Routes } from "react-router-dom";
-import { Search } from "./components/pages/Search";
 import { AppLayout } from "./layouts/AppLayout";
-import { SearchList } from "./pages/SearchList";
 
 function App() {
   return (
     <Routes>
-      <Route element={<AppLayout/>}>
-        <Route path="/"/>
-        <Route path="/home"/>
-        <Route path="/admin"/>
-        <Route path="/admin/new"/>
-        <Route path="/admin/edit"/>
-        <Route path="/search"/>
-        <Route path="/search/detail"/>
-        <Route path="/upload"/>
-        <Route path="/my/profile"/>
-        <Route path="/my/password"/>
+      <Route element={<AppLayout />}>
+        <Route path="/" />
+        <Route path="/home" />
+        <Route path="/admin" />
+        <Route path="/admin/new" />
+        <Route path="/admin/edit" />
+        <Route path="/search" />
+        <Route path="/search/detail" />
+        <Route path="/upload" />
+        <Route path="/my/profile" />
+        <Route path="/my/password" />
       </Route>
     </Routes>
   );
