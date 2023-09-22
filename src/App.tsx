dd930dcb6f74a5e175dacec20b2c50560cb4e243
Jin hyeon <kangjin627@gmail.com>
2023-09-22 15:58:52 +0900

    ✨:: 라우팅 설정 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import { AppLayout } from "./layouts/AppLayout";
+import { DetailPage } from "./pages/Detail";
 import NotFound from "./pages/NotFound";
 import { SearchPage } from "./pages/Search";
 import { SearchListPage } from "./pages/SearchList";
@@ -9,18 +10,18 @@ function App() {
     <Routes>
       <Route element={<AppLayout />}>
         <Route path="/" />
-        <Route path="/home" />
+        <Route path="/home" element={<SearchPage />} />
         <Route path="/admin" />
         <Route path="/admin/new" />
         <Route path="/admin/edit" />
-        <Route path="/search" element={<SearchPage />} />
-        <Route path="/search/detail" element={<SearchListPage />} />
+        <Route path="/search" element={<SearchListPage />} />
+        <Route path="/search/detail" element={<DetailPage />} />
         <Route path="/upload" />
         <Route path="/my/profile" />
         <Route path="/my/password" />
-        <Route path="*" element={<NotFound/>}/>
+        <Route path="*" element={<NotFound />} />
       </Route>
-    </Routes>
+    </Routes >
   );
 }
 
