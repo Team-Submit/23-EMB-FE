4f8b99ce84c10850a5c0b19b19b22540ad9ae9c0
kimgyuha <kimgyuha25@gmail.com>
2023-09-22 16:59:43 +0900

    💄:: Header 팝업 수정

--- src/components/common/Header/style.ts
+++ src/components/common/Header/style.ts
@@ -1,57 +1,66 @@
-import { Link } from 'react-router-dom';
-import { color } from './../../../styles/theme';
+import { Link } from "react-router-dom";
+import { color } from "./../../../styles/theme";
 import { styled } from "styled-components";
 
 export const Container = styled.div`
-  display: flex;  
+  display: flex;
   justify-content: space-between;
   padding: 12px 40px;
   min-width: 700px;
   border-bottom: 1px solid ${color.Gray[75]};
   position: sticky;
-  top:0;
+  top: 0;
   background-color: ${color.White};
   z-index: 100;
-`
+`;
 
 export const UserContainer = styled.div`
   display: flex;
   align-items: center;
   gap: 20px;
-`
+`;
 export const Name = styled.div`
-position: relative;
+  position: relative;
   display: flex;
+  align-items: center;
   gap: 4px;
 
   cursor: pointer;
 
-  >p{
+  > p {
     font-size: 24px;
   }
-`
+`;
+
+export const MenuContainer = styled.div`
+  display: flex;
+  gap: 8px;
+`;
+
+export const TabLink = styled(Link)`
+  text-decoration: none;
+`;
+
 export const SettingDropContainer = styled.div`
   position: absolute;
-  top:28px;
+  top: 30px;
   right: 0px;
+  z-index: 10;
 
+  display: flex;
+  flex-direction: column;
   padding: 6px 12px;
+  gap: 4px;
   width: 120px;
 
   background-color: ${color.White};
   border: 1px solid ${color.Gray[75]};
   border-radius: 4px;
 
-  >div{
+  > div {
     cursor: pointer;
   }
-`
-
-export const MenuContainer = styled.div`
-  display: flex;
-  gap: 8px;
-`
-
-export const TabLink =  styled(Link)`
-  text-decoration: none;
-`
+  > hr {
+    border: 1px solid ${color.Gray[85]};
+  }
+`;
