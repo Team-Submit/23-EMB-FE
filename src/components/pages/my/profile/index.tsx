10aed9d4db194f395935a1e546a367150da98bc4
phyuna0525 <128463899+phyuna0525@users.noreply.github.com>
2023-10-04 21:27:55 +0900

    💄:: 모달창 활성화 및 비밀번호 확인 생성

--- src/components/pages/my/profile/index.tsx
+++ src/components/pages/my/profile/index.tsx
@@ -3,19 +3,29 @@ import { Button } from "../../../../styles/common/Button";
 import { Input } from "../../../common/Input";
 import { Tab } from "../../../common/Tab";
 import * as S from "./style";
+import { InputAuto } from "../../../common/InputAuto";
+import { Modal } from "../../../common/Modal";
 
 interface changeProfileType {
-  newDepartment: string;
-  newUserName: string;
-  newUserNumber: string;
+  // newDepartment: string;
+  UserName: string;
+  UserNumber: string;
 }
 
 export const Profile = () => {
+  const [open, setOpen] = useState<boolean>(false);
   const [changeProfile, setChangeProfile] = useState<changeProfileType>({
-    newDepartment: "",
-    newUserName: "",
-    newUserNumber: "",
+    // newDepartment: "",
+    UserName: "",
+    UserNumber: "",
   });
+  const [Department, setDepartment] = useState("");
+
+  // instance.get("/informations", {
+  //   Department: "",
+  //   UserName: "",
+  //   UserNumber: "",
+  // });
 
   const onChangeProfile = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -24,6 +34,9 @@ export const Profile = () => {
       ...changeProfile,
       [name]: value,
     });
+  };
+  const openModal = () => {
+    setOpen(!open);
   };
 
   useEffect(() => {
@@ -57,25 +70,41 @@ export const Profile = () => {
         <S.personalText>개인정보 수정</S.personalText>
         <Input
           onChange={onChangeProfile}
-          value={changeProfile.newUserName}
+          value={changeProfile.UserName}
           label="이름"
           name="newUserName"
+          placeholder="이름을 입력해주세요"
         />
         <Input
           onChange={onChangeProfile}
-          value={changeProfile.newUserNumber}
+          value={changeProfile.UserNumber}
           label="연락처"
           name="newUserNumber"
+          placeholder="연락처를 입력해주세요"
         />
-        <Input
-          onChange={onChangeProfile}
-          value={changeProfile.newDepartment}
+        <InputAuto
+          value={Department}
+          setValue={setDepartment}
           label="부서"
-          name="newDepartment"
+          placeholder="부서를 입력해주세요"
         />
-        <Button size="XL" colorType="Point">
+        <Button size="XL" colorType="Point" onClick={openModal}>
           완료
         </Button>
+        {open && (
+          <Modal isOpen>
+            <S.ModifyMoadleTitle>수정하시겠습니까?</S.ModifyMoadleTitle>
+            <S.ModifyBody>이 작업은 되돌릴 수 없습니다</S.ModifyBody>
+            <S.ModifyMoadlBtnFlex>
+              <Button colorType="Point" size="M" onClick={() => {}}>
+                수정
+              </Button>
+              <Button colorType="Gray" size="M" onClick={openModal}>
+                취소
+              </Button>
+            </S.ModifyMoadlBtnFlex>
+          </Modal>
+        )}
       </S.profileInputWarp>
     </S.Background>
   );
--- src/components/pages/my/profile/style.ts
+++ src/components/pages/my/profile/style.ts
@@ -32,3 +32,23 @@ export const profileTabWarp = styled.div`
   margin-bottom: 50px;
   gap: 6px;
 `;
+
+export const ModifyMoadleTitle = styled.p`
+  font-size: 32px;
+  font-weight: 700;
+`;
+
+export const ModifyBody = styled.p`
+  font-size: 24px;
+  font-weight: 400;
+`;
+
+export const ModifyMoadlBtnFlex = styled.div`
+  width: 100%;
+  display: flex;
+  gap: 20px;
+
+  & > button {
+    width: 50%;
+  }
+`;
