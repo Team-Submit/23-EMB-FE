8dd0a7c667b8516e2c62769acb4daf449e025583
김상구 <spair0039@gmail.com>
2023-10-05 17:21:30 +0900

    💄:: 라디오 퍼블리싱

--- src/components/document/FileDownloadModal/index.tsx
+++ src/components/document/FileDownloadModal/index.tsx
@@ -1,16 +1,23 @@
 import { useState } from "react";
 import { Modal } from "../../common/Modal";
 import { Button } from "../../../styles/common/Button";
-import { ButtonHorizonal, Text24 } from "./style";
+import { ButtonHorizonal, RadioContainer, Text24 } from "./style";
+import Radio from "../../common/Radio";
 
 export default function FileDownloadModal(){
     const [IsOpen, setIsOpen] = useState<boolean>(true);
+    const [RadioSelect, setRadioSelect] = useState<string>('CerificationOfCareer');
 
     return(
         <Modal isOpen={IsOpen} title="다운로드">
             <Text24>
                 박씨(2077/09/23)
             </Text24>
+            <RadioContainer>
+                <Radio text="경력증명서" selected={RadioSelect === 'CerificationOfCareer'} onClick={()=>setRadioSelect('CerificationOfCareer')}/>
+                <Radio text="애플캣" selected={RadioSelect === 'applecat'} onClick={()=>setRadioSelect('applecat')}/>
+                <Radio text="테스트" selected={RadioSelect === 'testing'} onClick={()=>setRadioSelect('testing')}/>
+            </RadioContainer>
             <ButtonHorizonal>
                 <Button size='M' colorType="Point">다운로드</Button>
                 <Button size='M' colorType="Gray" onClick={()=> setIsOpen(false)}>닫기</Button>
