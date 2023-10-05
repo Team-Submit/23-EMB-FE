722b730a200a93c67832d2c489fd198b05068f4a
김상구 <spair0039@gmail.com>
2023-10-05 11:13:39 +0900

    💄:: 미사용 import 정리

--- src/components/common/Radio/style.ts
+++ src/components/common/Radio/style.ts
@@ -1,7 +1,3 @@
-import { Button } from "./../../../styles/common/Button";
-import { Check } from "./../../../assets/icons/Check";
-import { TransparentDiv } from "./../Data/style";
-import { hover } from "@testing-library/user-event/dist/hover";
 import { BoxShadow, color } from "./../../../styles/theme";
 import { styled, css } from "styled-components";
 
