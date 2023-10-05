25cc297d456b3e21ec65798ef89574a33b9278db
kimgyuha <kimgyuha25@gmail.com>
2023-10-05 18:44:58 +0900

    💄:: DepartmentInput 구현

--- src/components/common/InputAuto/index.tsx
+++ src/components/common/InputAuto/index.tsx
@@ -4,33 +4,26 @@ import * as S from "./style";
 import { hangulIncludes } from "@toss/hangul";
 import { Plus } from "../../../assets/icons/Plus";
 import { color } from "../../../styles/theme";
-import { getDepartment } from "../../../apis/common/department";
 
 interface InputAutoProps {
   label: string;
   placeholder?: string;
   value: string;
   setValue: React.Dispatch<React.SetStateAction<string>>;
+
+  list: string[];
 }
 
-/** < InputAuto value={값} setValue={set값} label={label} placeholder?={placeholder} /> */
-export const InputAuto = ({
+/** < InputAuto value={값} setValue={set값} list={문자 배열} label={label} placeholder?={placeholder} /> */
+export const InputAuto =({
   label,
   placeholder,
   value,
+  list,
   setValue,
 }: InputAutoProps) => {
   const selectListRef = useRef<HTMLDivElement | null>(null);
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const [list, setList] = useState<string[]>([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await getDepartment();
-      setList(res);
-    };
-    fetchData();
-  }, []);
 
   useEffect(() => {
     const handleOutsideClose = (e: { target: any }) => {
