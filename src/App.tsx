617e43a09b5fa43e6f992be0831af9187e7d26eb
taeyoooon <leteyune101010@gmail.com>
2023-10-05 22:53:35 +0900

    Auto stash before merge of "feature/Login" and "origin/feature/Login"

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import { AppLayout } from "./layouts/AppLayout";
 import NotFound from "./pages/NotFound";
 import { SearchPage } from "./pages/Search";
 import { SearchListPage } from "./pages/SearchList";
+import { Login } from "./pages/Login";
+import { FirstLogin } from "./pages/first_Login";
 
 function App() {
   return (
     <Routes>
       <Route element={<AppLayout />}>
-        <Route path="/" />
+        <Route path="/Login" element={<Login/>} />
+        <Route path="/FirstLogin" element={<FirstLogin/>} />
         <Route path="/home" />
         <Route path="/admin" />
         <Route path="/admin/new" />
