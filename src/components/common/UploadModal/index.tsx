424cb8c40f06ebf9d0a15b5432e14a18e4772dca
Jin hyeon <kangjin627@gmail.com>
2023-10-06 17:29:49 +0900

    💄:: 모달 컴포넌트 분리

---
+++ src/components/common/UploadModal/index.tsx
@@ -0,0 +1,20 @@
+import { Dispatch } from "react";
+import { Button } from "../../../styles/common/Button";
+import { Modal } from "../Modal";
+import * as S from './style';
+
+interface OpenPropsType {
+    OpenProps: boolean;
+    SetOpenProps: Dispatch<React.SetStateAction<boolean>>;
+}
+
+export const UploadModal = ({ OpenProps, SetOpenProps }: OpenPropsType) => {
+    return (
+        <Modal isOpen={OpenProps} title="인적사항을 업로드할까요?">
+            <S.MoadlBtnFlex>
+                <Button colorType="Point" size="XL" onClick={() => { }}>업로드</Button>
+                <Button colorType="Gray" size="XL" onClick={() => { SetOpenProps(false) }}>취소</Button>
+            </S.MoadlBtnFlex>
+        </Modal>
+    );
+}
