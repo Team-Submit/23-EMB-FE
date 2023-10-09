cff69178f0f321da237c8877f2150ff006949d75
Jin hyeon <kangjin627@gmail.com>
2023-10-09 20:12:06 +0900

    ✨ :: 충돌수정

--- src/components/common/DetailData/Last.tsx
+++ src/components/common/DetailData/Last.tsx
@@ -1,9 +1,6 @@
 import * as S from "./style";
 import { DepartmentBadge } from "../DepartmentBadge";
 
-<<<<<<< Updated upstream
-export const LastData = () => {
-=======
 interface LastType {
     department: string;
     userName: string;
@@ -11,23 +8,11 @@ interface LastType {
 }
 
 export const LastData = ({ department, userName, userNumber }: LastType) => {
->>>>>>> Stashed changes
     return (
         <S.BigFlex>
             <S.Title>등록당시</S.Title>
             <S.SmallFlex>
                 <S.Label>발급 부서</S.Label>
-<<<<<<< Updated upstream
-                <DepartmentBadge department="재정낭비" widthAuto />
-            </S.SmallFlex>
-            <S.SmallFlex>
-                <S.Label>담당자 이름</S.Label>
-                <S.Body>김밥봉</S.Body>
-            </S.SmallFlex>
-            <S.SmallFlex>
-                <S.Label>담당자 전화번호</S.Label>
-                <S.Body>010-1997-1015</S.Body>
-=======
                 <DepartmentBadge department={department} widthAuto />
             </S.SmallFlex>
             <S.SmallFlex>
@@ -37,7 +22,6 @@ export const LastData = ({ department, userName, userNumber }: LastType) => {
             <S.SmallFlex>
                 <S.Label>담당자 전화번호</S.Label>
                 <S.Body>{userNumber}</S.Body>
->>>>>>> Stashed changes
             </S.SmallFlex>
         </S.BigFlex>
 
