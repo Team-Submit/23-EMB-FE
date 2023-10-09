3f9c27733724beeb19990adc8e2b0374f589c97c
Jin hyeon <kangjin627@gmail.com>
2023-10-09 20:05:57 +0900

    ✨ :: 상세포기 페이지 api연동

--- src/components/common/DetailData/Last.tsx
+++ src/components/common/DetailData/Last.tsx
@@ -1,12 +1,23 @@
 import * as S from "./style";
 import { DepartmentBadge } from "../DepartmentBadge";
 
+<<<<<<< Updated upstream
 export const LastData = () => {
+=======
+interface LastType {
+    department: string;
+    userName: string;
+    userNumber: string;
+}
+
+export const LastData = ({ department, userName, userNumber }: LastType) => {
+>>>>>>> Stashed changes
     return (
         <S.BigFlex>
             <S.Title>등록당시</S.Title>
             <S.SmallFlex>
                 <S.Label>발급 부서</S.Label>
+<<<<<<< Updated upstream
                 <DepartmentBadge department="재정낭비" widthAuto />
             </S.SmallFlex>
             <S.SmallFlex>
@@ -16,6 +27,17 @@ export const LastData = () => {
             <S.SmallFlex>
                 <S.Label>담당자 전화번호</S.Label>
                 <S.Body>010-1997-1015</S.Body>
+=======
+                <DepartmentBadge department={department} widthAuto />
+            </S.SmallFlex>
+            <S.SmallFlex>
+                <S.Label>담당자 이름</S.Label>
+                <S.Body>{userName}</S.Body>
+            </S.SmallFlex>
+            <S.SmallFlex>
+                <S.Label>담당자 전화번호</S.Label>
+                <S.Body>{userNumber}</S.Body>
+>>>>>>> Stashed changes
             </S.SmallFlex>
         </S.BigFlex>
 
