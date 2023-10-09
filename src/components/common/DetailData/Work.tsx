160bc1e8ad643da81092d5f1de80bcfffa60ba49
Jin hyeon <kangjin627@gmail.com>
2023-10-09 20:05:57 +0900

    ✨ :: 상세포기 페이지 api연동

--- src/components/common/DetailData/Work.tsx
+++ src/components/common/DetailData/Work.tsx
@@ -1,11 +1,25 @@
 import * as S from "./style";
 
+<<<<<<< Updated upstream
 export const WorkData = () => {
+=======
+interface WorkType {
+    name: string,
+    birthdate: string,
+    address: string,
+    firstTenure: string,
+    lastTenure: string,
+    occupation: string,
+}
+
+export const WorkData = ({ name, birthdate, address, firstTenure, lastTenure, occupation }: WorkType) => {
+>>>>>>> Stashed changes
     return (
         <S.BigFlex>
             <S.Title>근무자</S.Title>
             <S.SmallFlex>
                 <S.Label>성명</S.Label>
+<<<<<<< Updated upstream
                 <S.Body>김바보</S.Body>
             </S.SmallFlex>
             <S.SmallFlex>
@@ -23,6 +37,25 @@ export const WorkData = () => {
             <S.SmallFlex>
                 <S.Label>직종</S.Label>
                 <S.Body>지구 파괴</S.Body>
+=======
+                <S.Body>{name}</S.Body>
+            </S.SmallFlex>
+            <S.SmallFlex>
+                <S.Label>생년월일</S.Label>
+                <S.Body>{birthdate}</S.Body>
+            </S.SmallFlex>
+            <S.SmallFlex>
+                <S.Label>주소</S.Label>
+                <S.Body>{address}</S.Body>
+            </S.SmallFlex>
+            <S.SmallFlex>
+                <S.Label>재직기간</S.Label>
+                <S.Body>{firstTenure} ~ {lastTenure}</S.Body>
+            </S.SmallFlex>
+            <S.SmallFlex>
+                <S.Label>직종</S.Label>
+                <S.Body>{occupation}</S.Body>
+>>>>>>> Stashed changes
             </S.SmallFlex>
         </S.BigFlex>
 
--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,15 +1,43 @@
-import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getDetail } from "../../apis/common/detail";
 import { ChevronLeft20 } from "../../assets/icons/ChevronLeft20";
 import { DownLoad } from "../../assets/icons/DownLoad";
 import { LastData } from "../../components/common/DetailData/Last";
 import { NowData } from "../../components/common/DetailData/Now";
 import { WorkData } from "../../components/common/DetailData/Work";
-import FileDownloadModal from "../../components/document/FileDownloadModal";
 import { Button } from "../../styles/common/Button";
 import * as S from "./style";
 
 export const DetailPage = () => {
+import FileDownloadModal from "../../components/document/FileDownloadModal";
+import { Button } from "../../styles/common/Button";
+import * as S from "./style";
+
+interface DetailType {
+    name: string,
+    birthdate: string,
+    address: string,
+    firstTenure: string,
+    lastTenure: string,
+    occupation: string,
+    department: string,
+    userName: string,
+    userNumber: string
+}
+
+export const DetailPage = () => {
     const [IsDownloadModalOpen, setIsDownloadModalOpen] = useState<boolean>(false);
+    const [detailData, setDetailData] = useState<DetailType>();
+    const param = useParams();
+
+    console.log(param.id);
+
+    useEffect(() => {
+        const data = getDetail(`${param.id}`);
+        // setDetailData(data); api연동시 변경 예정
+    }, []);
+>>>>>>> Stashed changes
 
     return (
         <S.Backgound>
@@ -18,14 +46,36 @@ export const DetailPage = () => {
                 <S.BackName >홍길동이</S.BackName>
             </S.BackPage>
             <div>
+<<<<<<< Updated upstream
                 <WorkData />
                 <S.Line />
                 <LastData />
                 <S.Line />
                 <NowData />
             </div>
-            <Button size="XL" colorType="Point" onClick={()=>setIsDownloadModalOpen(true)}><DownLoad fill="white" />다운로드</Button>
-            <FileDownloadModal IsOpen={IsDownloadModalOpen} setIsOpen={setIsDownloadModalOpen}/>
+            <Button size="XL" colorType="Point" ><DownLoad fill="white" />다운로드</Button>
+                {/* <WorkData
+                    name={detailData.name}
+                    birthdate={detailData.birthdate}
+                    firstTenure={detailData.firstTenure}
+                    lastTenure={detailData.lastTenure}
+                    occupation={detailData.occupation}
+                /> */}
+                <S.Line />
+                {/* <LastData 
+                    userName={detailData.userName}
+                    userNumber={detailData.userNumber}
+                /> */}
+                    department={detailData.department}
+                <S.Line />
+                    userName={detailData.userName}
+                {/* <NowData 
+                    userNumber={detailData.userNumber}
+                /> */}
+            <Button size="XL" colorType="Point" onClick={() => setIsDownloadModalOpen(true)}><DownLoad fill="white" />다운로드</Button>
+            </div>
+                    department={detailData.department}
+            <FileDownloadModal IsOpen={IsDownloadModalOpen} setIsOpen={setIsDownloadModalOpen} />
         </S.Backgound>
     );
 }
