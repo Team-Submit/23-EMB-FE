449a78bcfa0a0976834515065c4c01236f26a63c
한예슬 <yangsooh386@gmail.com>
2023-10-25 16:45:02 +0900

    🐛:: 계정 삭제 경로 수정

--- src/apis/common/delAccounts.tsx
+++ src/apis/common/delAccounts.tsx
@@ -2,7 +2,7 @@ import { instance } from ".";
 
 export const delAccounts = async () => {
   try {
-    await instance.delete("/users/accounts");
+    await instance.delete("/manager/accounts");
   } catch (err) {
     console.log("계정 삭제에 실패했습니다. 관리자에게 문의해주세요");
   }
