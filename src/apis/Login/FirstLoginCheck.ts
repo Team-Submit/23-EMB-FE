412298e03ddba9dd47a7063d9a03c652cadd9d4e
Jin hyeon <kangjin627@gmail.com>
2023-12-05 16:49:38 +0900

    🎨:: 로그인부분 api연동 끝

--- src/apis/Login/FirstLoginCheck.ts
+++ src/apis/Login/FirstLoginCheck.ts
@@ -6,7 +6,7 @@ export interface LoginCredentials {
 
 export const LoginCheck = async ({username}: LoginCredentials) =>{
     try{
-        const response = await instance.post('/users/firstLoginCheck', {userid: username});
+        const response = await instance.patch('/users/firstLoginCheck', {userid: username});
         return response.data;
     }catch(error){
         throw error;
