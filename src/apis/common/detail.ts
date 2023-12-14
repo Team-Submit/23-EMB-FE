1981a106caf47031b63fcae7f02671541cda969a
Jin hyeon <kangjin627@gmail.com>
2023-12-14 09:01:12 +0900

    ✨:: 상세보기 uri주소 변경

--- src/apis/common/detail.ts
+++ src/apis/common/detail.ts
@@ -2,7 +2,7 @@ import { instance } from "..";
 
 export const getDetail = async (id:string) => {
   try {
-    const data = await instance.get(`/information/details${id}`);
+    const data = await instance.get(`/information/details/${id}`);
     return data.data;
   } catch(err) {
     console.log("상세정보를 불러오는데 오류가 생겼습니다.");
