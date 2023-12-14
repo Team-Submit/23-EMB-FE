52dd8b0f8d734100117b9b9924791e7241332930
kimgyuha <kimgyuha25@gmail.com>
2023-12-14 19:12:55 +0900

    🐛:: 스크롤 에러 수정

--- src/components/common/Modal/index.tsx
+++ src/components/common/Modal/index.tsx
@@ -10,43 +10,61 @@ interface ModalProps {
 }
 
 /** 사용법 : < Modal isOpen={boolean} title?="title"> {children} </ Modal > */
-export const Modal = ({ isOpen, setOpen, children, title, lowImportance }: ModalProps) => {
+export const Modal = ({
+  isOpen,
+  setOpen,
+  children,
+  title,
+  lowImportance,
+}: ModalProps) => {
   useEffect(() => {
-    document.body.style.cssText = `
+    if (isOpen) {
+      document.body.style.cssText = `
       position: fixed; 
       top: -${window.scrollY}px;
       overflow-y: scroll;
       width: 100%;`;
-    document.addEventListener('keydown', LowImportanceKeydownHandler)
+      document.addEventListener("keydown", LowImportanceKeydownHandler);
 
-    return () => {
-      const scrollY = document.body.style.top;
-      document.body.style.cssText = "";
-      window.scrollTo(0, parseInt(scrollY || "0", 10) * -1);
-      document.removeEventListener('keydown', LowImportanceKeydownHandler)
-    };
+      return () => {
+        const scrollY = document.body.style.top;
+        document.body.style.cssText = "";
+        window.scrollTo(0, parseInt(scrollY || "0", 10) * -1);
+        document.removeEventListener("keydown", LowImportanceKeydownHandler);
+      };
+    }
   }, []);
 
-  function LowImportanceBackgroudClickHandler(e:React.MouseEvent<HTMLDivElement>){
-    if(setOpen && lowImportance && e.target instanceof HTMLDivElement && e.target.id === "ModalBackdrop"){
-      setOpen(false)
+  function LowImportanceBackgroudClickHandler(
+    e: React.MouseEvent<HTMLDivElement>
+  ) {
+    if (
+      setOpen &&
+      lowImportance &&
+      e.target instanceof HTMLDivElement &&
+      e.target.id === "ModalBackdrop"
+    ) {
+      setOpen(false);
     }
   }
 
-  function LowImportanceKeydownHandler(e:any){
-    if(setOpen && lowImportance && e.key === 'Escape'){
-      setOpen(false)
+  function LowImportanceKeydownHandler(e: any) {
+    if (setOpen && lowImportance && e.key === "Escape") {
+      setOpen(false);
     }
   }
 
   return (
     <>
       {isOpen && (
-        <S.BackDrop id="ModalBackdrop" onClick={LowImportanceBackgroudClickHandler}>
+        <S.BackDrop
+          id="ModalBackdrop"
+          onClick={LowImportanceBackgroudClickHandler}
+        >
           <S.Container>
             {title && <S.Title>{title}</S.Title>}
             {children}
-            </S.Container>
+          </S.Container>
         </S.BackDrop>
       )}
     </>
