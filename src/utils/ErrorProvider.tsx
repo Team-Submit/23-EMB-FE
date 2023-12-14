5b0edf037d31ad87c7b97cb15c85faf410c1ea5f
김상구 <spair0039@gmail.com>
2023-12-14 14:09:17 +0900

    ✨:: ErrorProvider 개발

---
+++ src/utils/ErrorProvider.tsx
@@ -0,0 +1,41 @@
+import React, { Dispatch, SetStateAction, createContext, useContext, useState } from "react";
+import { Modal } from "../components/common/Modal";
+import { ButtonHorizonal } from "../components/document/FileDownloadModal/style";
+import { Button } from "../styles/common/Button";
+
+const ErrorContext = createContext({} as ContextPropsType);
+
+interface ContextPropsType{
+    state: string;
+    setState: Dispatch<SetStateAction<string>>
+}
+
+interface ErrorProviderType{
+    children: React.ReactNode
+};
+
+export function ErrorProvider({children}:ErrorProviderType){
+    const [message, setMessage] = useState<string>('');
+    const ProviderValue = {
+        state: message,
+        setState: setMessage
+    };
+
+    return(
+        <ErrorContext.Provider value={ProviderValue}>
+            {message!=='' &&
+                <Modal isOpen title="오류" lowImportance>
+                    {message}
+                    <ButtonHorizonal>
+                        <Button size="M" colorType="Gray" onClick={()=>setMessage('')}>닫기</Button>
+                    </ButtonHorizonal>
+                </Modal>
+            }
+            {children}
+        </ErrorContext.Provider>
+    );
+};
+
+export function useError(){
+    return useContext(ErrorContext);
+}
